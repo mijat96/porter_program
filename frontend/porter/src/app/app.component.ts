764067f16fc134a9413da3f6372ef0cf778102c9
mijat96 <marko_srb@hotmail.rs>
2022-09-11 16:46:59 +0200

    init login design

--- frontend/porter/src/app/app.component.ts
+++ frontend/porter/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'porter';
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      recaptcha: ''
+    })
+  }
 }
